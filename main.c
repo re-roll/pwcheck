e19440c54a8820e71fb28e98d96eae8838e25283
破壊力 <74363032+re-roll@users.noreply.github.com>
2021-10-25 11:23:44 +0200

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int strLen (char str[max_len])
     return i;
 }
 
-int par_1 (char str[max_len])
+int lowerCase (char str[max_len])
 {
     for (int i = 0; i < max_len; i++)
     {
@@ -24,11 +24,11 @@ int par_1 (char str[max_len])
     return 0;
 }
 
-int par_2 (char str[max_len])
+int upperCase (char str[max_len])
 {
     for (int i = 0; i < max_len; i++)
     {
-        if ((par_1(str)) && ((str[i] >= 'A') && (str[i] <= 'Z')))
+        if ((str[i] >= 'A') && (str[i] <= 'Z'))
             return 1;
         if (str[i] == '\0')
             break;
@@ -36,11 +36,11 @@ int par_2 (char str[max_len])
     return 0;
 }
 
-int par_3 (char str[max_len])
+int numSym (char str[max_len])
 {
     for (int i = 0; i < max_len; i++)
     {
-        if ((par_2(str)) && ((str[i] >= '0') && (str[i] <= '9')))
+        if ((str[i] >= '0') && (str[i] <= '9'))
             return 1;
         if (str[i] == '\0')
             break;
@@ -48,11 +48,11 @@ int par_3 (char str[max_len])
     return 0;
 }
 
-int par_4 (char str[max_len])
+int specSym (char str[max_len])
 {
     for (int i = 0; i < max_len; i++)
     {
-        if ((par_3(str)) && (((str[i] > 31) && (str[i] < 48)) || ((str[i] > 57) && (str[i] < 65)) || ((str[i] > 122) && (str[i] < 127))))
+        if (((str[i] > 31) && (str[i] < 48)) || ((str[i] > 57) && (str[i] < 65)) || ((str[i] > 122) && (str[i] < 127)))
             return 1;
         if (str[i] == '\0')
             break;
@@ -63,9 +63,9 @@ int par_4 (char str[max_len])
 int lvl_1 (char str[max_len])
 {
     int k = 0;
-    if (par_1(str))
+    if (lowerCase(str))
         k++;
-    if (par_2(str))
+    if (upperCase(str))
         k++;
     
     if (k == 2)
@@ -76,16 +76,16 @@ int lvl_1 (char str[max_len])
 
 int lvl_2 (int p, char str[max_len])
 {
-    if ((p == 1) && (par_1(str)))
+    if ((p == 1) && (lvl_1(str)))
         return 1;
-    
-    if ((p == 2) && (par_2(str)))
-        return 1;
-
-    if ((p == 3) && (par_3(str)))
+        
+    if ((p == 2) && (lvl_1(str)))
         return 1;
 
-    if ((p >= 4) && (par_4(str)))
+    if ((p == 3) && ((lvl_1(str) && numSym(str)) || (lvl_1(str) && specSym(str))))
+        return 1;
+
+    if ((p >= 4) && ((lvl_1(str) && numSym(str) && specSym(str))))
         return 1;
 
     return 0;
@@ -112,6 +112,11 @@ int main(int argc, char* argv[])
     int lvl = 0;
     int par = 0;
     int stats = 0;
+
+    for (int i = 0; i < argc; i++)
+    {
+        printf("%s", argv[i]);
+    }
 
     printf("Level: %d\n", lvl);
     printf("Parameter: %d\n", par);
